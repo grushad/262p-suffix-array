5f0d2b5a7e49aca34716bcb23b5f352f40cc8b1c
grushad <grushadharod@gmail.com>
2023-05-18 15:42:18 -0700

    initial commit

---
+++ project5/SuffixArray.java
@@ -0,0 +1,15 @@
+package project5;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.ArrayList;
+
+public class SuffixArray {
+  
+  public ArrayList<Integer> construct(String S) {
+    // Your code goes here
+    // The function should return a vector of integers storing the integer IDs of the suffix array
+    return null;
+  }
+
+}
